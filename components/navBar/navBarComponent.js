252c6ef113c92baef045ccd509106fc2b7f8da76
Alison.F <alisonqueiroz_@hotmail.com>
2024-09-14 15:57:01 -0300

    limpando linha de codigo

--- components/navBar/navBarComponent.js
+++ components/navBar/navBarComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, C } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import styles from './styles';
 
