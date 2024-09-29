ff29ff31a64004ab21778b874d22d98cb81cfd81
Luis Monteiro <luismonteirodeveloper@gmail.com>
2024-09-29 18:45:43 -0300

    feat: exportar dados do financeiro

--- screens/financeiro/financeiroScreen.js
+++ screens/financeiro/financeiroScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { Alert, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import RNHTMLtoPDF from 'react-native-html-to-pdf';
 import styles from './styles';
 
 export default function FinanceiroScreen({ navigation }) {
@@ -22,21 +23,25 @@ export default function FinanceiroScreen({ navigation }) {
           AsyncStorage.getItem('rendaFixa'),
           AsyncStorage.getItem('outrasRendas'),
           AsyncStorage.getItem('despesas'),
+          AsyncStorage.getItem('lucroTotal'),
+          AsyncStorage.getItem('despesasTotal'),
         ]);
-
+  
         if (values[0] !== null) setSalario(values[0]);
         if (values[1] !== null) setEducacao(values[1]);
         if (values[2] !== null) setRendaFixa(values[2]);
         if (values[3] !== null) setOutrasRendas(values[3]);
         if (values[4] !== null) setDespesas(values[4]);
+        if (values[5] !== null) setLucroTotal(parseFloat(values[5]));
+        if (values[6] !== null) setDespesasTotal(parseFloat(values[6]));
       } catch (error) {
         console.log("Erro ao carregar dados armazenados", error);
       }
     };
-
+  
     loadValues();
   }, []);
-
+  
   const calcularReceita = async () => {
     if (!salario || !educacao || !rendaFixa || !outrasRendas || !despesas) {
       setErrorMessage('Por favor, preencha todos os campos.');
@@ -68,6 +73,43 @@ export default function FinanceiroScreen({ navigation }) {
   const handleChange = async (setter, key, value) => {
     setter(value);
     await AsyncStorage.setItem(key, value);
+  };
+
+  const generatePDF = async (salario, educacao, rendaFixa, outrasRendas, despesas, lucro, despesa) => {
+    const htmlContent = `
+      <h1>Resumo Financeiro</h1>
+      <p><strong>Salário:</strong> R$ ${salario}</p>
+      <p><strong>Gastos com Educação:</strong> R$ ${educacao}</p>
+      <p><strong>Renda Fixa:</strong> R$ ${rendaFixa}</p>
+      <p><strong>Outras Rendas:</strong> R$ ${outrasRendas}</p>
+      <p><strong>Despesas:</strong> R$ ${despesas}</p>
+      <p><strong>Lucro Total:</strong> R$ ${lucro}</p>
+      <p><strong>Despesas Totais:</strong> R$ ${despesa}</p>
+    `;
+
+    console.log(
+      'Salario' + salario,
+      'Educacao' + educacao,
+      'RendaFixa' + rendaFixa,
+      'OutrasRendas' + outrasRendas,
+      'Despesas' + despesas,
+      'LucroTotal' + lucro,
+      'DespesasTotal' + despesa
+    )
+    console.log(RNHTMLtoPDF);
+
+    try {
+      const options = {
+        html: htmlContent,
+        fileName: 'resumo_financeiro',
+        directory: 'Documents',
+      };
+
+      const file = await RNHTMLtoPDF.convert(options);
+      Alert.alert('Sucesso', `PDF gerado em: ${file.filePath}`);
+    } catch (error) {
+      console.error('Erro ao gerar PDF:', error);
+    }
   };
 
   return (
@@ -132,9 +174,32 @@ export default function FinanceiroScreen({ navigation }) {
         </TouchableOpacity>
       </View>
 
+      <Text style={styles.label}>Lucro Total</Text>
+      <TextInput 
+        style={styles.input} 
+        value={lucroTotal !== null ? lucroTotal.toFixed(2) : 0} 
+        editable={false}
+      />
+
+      <Text style={styles.label}>Despesas Totais</Text>
+      <TextInput 
+        style={styles.input} 
+        value={despesasTotal !== null ? despesasTotal.toFixed(2) : 0} 
+        editable={false}
+      />
+
       {lucroTotal !== null && (
         <Text style={styles.result}>Receita Total: R$ {lucroTotal.toFixed(2)}</Text>
       )}
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => generatePDF(salario, educacao, rendaFixa, outrasRendas, despesas, lucroTotal, despesasTotal)}
+          style={styles.buttonCalculate}
+        >
+          <Text style={styles.buttonText}>Exportar para PDF</Text>
+        </TouchableOpacity>
+      </View>
 
       <View style={styles.buttonContainer}>
         <TouchableOpacity
